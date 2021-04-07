6b2efe82adcac057d50b4d38d9135c890fc6d2c9
fengqiangzhao <ryenzhao@qq.com>
2021-04-07 10:37:00 +0800

    add java solutions P15 doc

--- java/src/main/java/io/algocasts/P15.java
+++ java/src/main/java/io/algocasts/P15.java
@@ -1,5 +1,12 @@
 package io.algocasts;
 
+/**
+ * 缺失的数字
+ * leetcode:196,268
+ * Tag:数组、数学、位操作
+ * Level:易
+ * 相同的数字【异或】的结果为0
+ */
 public class P15 {
     //Time:O(n) Space(O(1))
     public int missingNumber(int[] nums) {
